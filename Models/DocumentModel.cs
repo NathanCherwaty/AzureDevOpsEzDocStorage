4ae351a14058bb55bb85ef8ea3b9aaca22644ec7
Desmond Ku <anime.onii-chan_@hotmail.ca>
2020-09-14 16:14:29 -0600

    Added DocumentModel

---
+++ Models/DocumentModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EZDocStorage.Models
+{
+    public class DocumentModel
+    {
+        public int Id { get; set; }
+
+        public string DocName { get; set; }
+        public int Btyes { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string Extention { get; set; }
+    }
+}

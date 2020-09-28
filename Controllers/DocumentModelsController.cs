daea77229ba96095b03cce0eda2e4f42162dd017
Desmond Ku <anime.onii-chan_@hotmail.ca>
2020-09-28 14:42:10 -0600

    swagger and xml documentation

--- Controllers/DocumentModelsController.cs
+++ Controllers/DocumentModelsController.cs
@@ -32,8 +32,11 @@ namespace EZDocStorage.Controllers
         /// <summary>
         /// Gets a specific Document.
         /// </summary>
+        /// <param name="id">Id of the Document you are trying to get</param>
+        /// <response code="404">Document not found</response> 
         // GET: api/DocumentModels/5
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<DocumentModel>> GetDocumentModel(int id)
         {
             var documentModel = await _context.Documents.FindAsync(id);
@@ -49,10 +52,30 @@ namespace EZDocStorage.Controllers
         /// <summary>
         /// Updates a specific Document.
         /// </summary>
+        /// /// <remarks>
+        /// Sample request:
+        ///
+        ///     PUT /Documents
+        ///     {
+        ///        "id": 1,
+        ///        "docName": "Item1",
+        ///        "btyes": 64,
+        ///        "creationDate": "2000-01-30T14:00:00.000Z",
+        ///        "extention": "string"
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="id">Id of the Document you are trying to update must match in request and exist in database</param>
+        /// <param name="documentModel"></param>
+        /// <returns>updates a Document</returns>
+        /// <response code="204">Returns no content but Document was updated</response>
+        /// <response code="400">If the Document is null</response> 
         // PUT: api/DocumentModels/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<IActionResult> PutDocumentModel(int id, DocumentModel documentModel)
         {
             if (id != documentModel.Id)
@@ -84,10 +107,29 @@ namespace EZDocStorage.Controllers
         /// <summary>
         /// Creates a specific Document.
         /// </summary>
+        /// <remarks>
+        /// Sample request:
+        ///
+        ///     POST /Documents
+        ///     {
+        ///        "id": 1,
+        ///        "docName": "Item1",
+        ///        "btyes": 64,
+        ///        "creationDate": "2000-01-30T14:00:00.000Z",
+        ///        "extention": "string"
+        ///     }
+        ///
+        /// </remarks>
+        /// <param name="documentModel"></param>
+        /// <returns>A newly created Document</returns>
+        /// <response code="201">Returns the newly created Document</response>
+        /// <response code="400">If the item is null</response>            
         // POST: api/DocumentModels
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public async Task<ActionResult<DocumentModel>> PostDocumentModel(DocumentModel documentModel)
         {
             _context.Documents.Add(documentModel);
@@ -100,8 +142,11 @@ namespace EZDocStorage.Controllers
         /// <summary>
         /// Deletes a specific Document.
         /// </summary>
+        /// <param name="id">Id of the document you wish to delete</param>
+        /// <response code="404">Document not found</response>       
         // DELETE: api/DocumentModels/5
         [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         public async Task<ActionResult<DocumentModel>> DeleteDocumentModel(int id)
         {
             var documentModel = await _context.Documents.FindAsync(id);

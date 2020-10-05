6565142ec3f8f5428d3e8f01648fb2a844d9e40c
Desmond Ku <anime.onii-chan_@hotmail.ca>
2020-10-05 14:25:19 -0600

    Clean up of weather API, Clean up of Document API/Swagger

--- Models/DocumentModel.cs
+++ Models/DocumentModel.cs
@@ -2,17 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using Newtonsoft.Json;
 
 namespace EZDocStorage.Models
 {
     public class DocumentModel
     {
         public int Id { get; set; }
-
+        [Required]
+        [JsonProperty(PropertyName = "name")]
         public string DocName { get; set; }
-        public int Btyes { get; set; }
+        [Required]
+        public string Bytes { get; set; }
         public DateTime CreationDate { get; set; }
-        public string Extention { get; set; }
+        [Required]
+        public string Extension { get; set; }
     }
 
     public class DocumentModelDTO
